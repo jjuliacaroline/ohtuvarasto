c5a4f2fb56337794f447aa701efd36a3fe5c65b1
Julia <j.roukala95@gmail.com>
2024-11-02 16:20:48 +0200

    Added tests for 100% coverage

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,37 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_lisaa_varastoon_negatiivinen(self):
+        self.varasto.lisaa_varastoon(-2)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_negatiivinen_otto(self):
+        saatu_maara = self.varasto.ota_varastosta(-2)
+        self.assertAlmostEqual(saatu_maara, 0)
+
+    def test_lisaa_liikaa(self):
+        self.varasto.lisaa_varastoon(20)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ota_liikaa(self):
+        self.varasto.lisaa_varastoon(8)
+        saatu_maara = self.varasto.ota_varastosta(10)
+        self.assertAlmostEqual(saatu_maara, 8)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+    
+    def test_str(self):
+        varasto = Varasto(10, 0)
+        self.assertAlmostEqual(str(varasto), "saldo = 0, vielä tilaa 10")
+
+    def test_negatiivinen_tilavuus(self):
+        varasto = Varasto(-1)
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_negatiivinen_alkusaldo(self):
+        varasto = Varasto(10, -1)
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_alkusaldo_suurempi_kuin_tilavuus(self):
+        varasto = Varasto(10, 20)
+        self.assertAlmostEqual(varasto.saldo, 10)
